fcedfefebadfe5463b578d31a65f8da5b96ae18d
Timo Betcke <tbetcke@smlx14.(none)>
2010-09-21 15:02:23 +0100

    Added Metis Patch

--- get_metis.sh
+++ get_metis.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/sh
 wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-4.0.tar.gz
 tar xzvf ./metis-4.0.tar.gz
-cd metis-4.0/Lib
+cd metis-4.0
+patch -p1 < ../metis-4.0.patch
+cd Lib
 cp *.c ../../src/
 cp *.h ../../src/
+cd ../../
 
 
 
--- src/swig_headers.h
+++
@@ -1,8 +0,0 @@
-#ifndef _SWIG_HEADERS_H
-#define _SWIG_HEADERS_H
-
-void METIS_PartMeshDual(int *, int *, int *, int *, int *, int *, int *, int *, int *);
-void METIS_PartMeshNodal(int *, int *, int *, int *, int *, int *, int *, int *, int *);
-
-
-#endif
